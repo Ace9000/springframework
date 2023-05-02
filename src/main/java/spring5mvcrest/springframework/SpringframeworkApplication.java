74003c26a86f64ccf367c9f60c93c1c5057b94ce
Anastas Acevski <anastas.acevski@outlook.com>
2023-05-02 09:22:31 +0200

    Initial commit

---
+++ src/main/java/spring5mvcrest/springframework/SpringframeworkApplication.java
@@ -0,0 +1,13 @@
+package spring5mvcrest.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringframeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringframeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring5mvcrest/springframework/SpringframeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package spring5mvcrest.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringframeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
